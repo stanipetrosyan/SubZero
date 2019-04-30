fc33a3cbe9b23f9920a61652b8954751e40d0f4f
Stanislav <stani.petrosyan@gmail.com>
2019-04-30 22:35:00 +0200

    Added dialog style

--- src/browsers/git/git_renderer.js
+++ src/browsers/git/git_renderer.js
@@ -4,14 +4,24 @@ const git = require('../../lib/git-cli');
 
 
 const project = ipcRenderer.sendSync('project-request');
-console.log(project);
+
+
+
+let openDialog = document.getElementById('openDialog');
+let dialogWindow = document.getElementById('dialogWindow');
+//let submitBtn = document.getElementById('submit');
+
+openDialog.addEventListener('click', _ => {
+    dialogWindow.showModal();
+})
+
 git.getCommitList(project['path']).then(res => {
     console.log(res);
 });
 
 git.status(project['path']).then(res => {
     console.log(res);
-});
+})
 
 document.getElementById("cancel").addEventListener('click', _=>{
     ipcRenderer.send("close-modal");
