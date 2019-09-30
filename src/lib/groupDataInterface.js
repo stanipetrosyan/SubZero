3ba3c586f21d6e44f11428d528e2a816b93bf7b3
StaniPetrosyan <stani.petrosyan@gmail.com>
2019-09-30 15:49:32 +0200

    finish group interface

--- src/lib/groupDataInterface.js
+++ src/lib/groupDataInterface.js
@@ -1,8 +1,10 @@
 const key = 'groups';
 
 module.exports = {
+
     /**
-     * @param {object} group 
+     * @param {object} group
+     * @param {Array} store
      */
     addGroup(group, store){
         let groups = store.get(key);
@@ -13,60 +15,75 @@ module.exports = {
             return false;
         }
     },
+
     /**
      * @param {string} group_name
+     * @param {Array} store
      */
-    deleteGroup(group_name){
-        this.data = this.data.filter(elem => elem['name'] !== group_name)
-
-        return this.saveData()
-    },
-
-    /**
-     * @param {object} project 
-     */
-    addProject(project){
-        let index = this.findGroupByName(project['group']);
-        this.data[index].projects.push(project);
-
-        return this.saveData();
-    },
-
-    /**
-     * @param {object} project 
-     */
-    removeProject(project){
-        let index = this.findGroupByName(project['group']);
-        this.data[index].projects = this.data[index].projects.filter(elem => elem['name'] !== project['name'])
-        
-        return this.saveData();
+    removeGroup(group_name, store){
+        store.set(key, removeGroupByName(store.get(key), group_name));
     },
 
     /**
      * @param { object } old 
      * @param { object } update 
+     * @param {Array} store
      */
-    updateProject(old, update){
-        this.removeProject(old);
-        this.addProject(update);
+    updateGroup(old, update, store){
+        let groups = store.get(key);
+        let index = this.findGroupByName(groups, old.name);
+        groups[index] = update;
+        store.set(key);
     },
 
     /**
-     * @param { object } old 
-     * @param { object } update 
-     */
-    updateGroup(old, update){
-        let index = this.findGroupByName(old.name);
-        this.data[index] = update;
-
-        return this.saveData();
-    },
-
-    /**
+     * @param {Array} datas
      * @param {string} group_name 
      * @returns {number} index of group
      */
     findGroupByName(data, group_name){
         return data.findIndex(elem => elem['name'] == group_name);
+    },
+
+    /**
+     * @param {Array} data 
+     * @param {string} group_name 
+     * @returns {Array} data without element
+     */
+    removeGroupByName(data, group_name) {
+        return data.filter(elem => elem['name'] !== group_name)
+    },
+    
+    /**
+     * @param {object} project 
+     */
+    addProject(project, store){
+        let groups = store.get(key);
+        let index = this.findGroupByName(groups, project['group']);
+        groups[index].projects.push(project);
+        store.set(groups);
+    },
+
+    /**
+     * @param {object} project 
+     */
+    removeProject(project, store){
+        let index = this.findGroupByName(project['group']);
+        let groups = store.get(key);
+        groups[index].projects = this.removeProjectByName(groups[index], project['name'])
+        store.set(key, groups);
+    },
+
+    removeProjectByName(data, project_name) {
+        return data.projects.filter(elem => elem['name'] !== project_name)
+    },
+
+    /**
+     * @param { object } old 
+     * @param { object } update 
+     */
+    updateProject(store, old, update){
+        this.removeProject(old, store);
+        this.addProject(update, store);
     }
 }
