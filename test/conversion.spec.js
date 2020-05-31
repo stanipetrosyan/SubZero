91a455a34de7421d1d24d1cff81878ade158bf88
Stanislav <stani.petrosyan@gmail.com>
2020-05-31 21:17:41 +0200

    added conversion

---
+++ test/conversion.spec.js
@@ -0,0 +1,11 @@
+const assert = require('assert')
+const conversion = require('../app/lib/conversion')
+
+
+
+describe('some conversion test',() => {
+    it('should convert from bytes in human readable size', () => {
+        let result = conversion.convertBytes(2024000);
+        assert.equal(result, '1.9 MB');
+    })
+})
