dbdec88c5d244c6d3974c2626495336f4a5c6d09
Stanislav <stani.petrosyan@gmail.com>
2020-07-31 20:14:23 +0200

    fixed tests

--- test/editor-checker.spec.js
+++ test/editor-checker.spec.js
@@ -5,13 +5,13 @@ const checker = require('../app/lib/editor-checker');
 
 // Obv this test pass only if you have VSCode or Atom installed (in case check your global variable)
 describe('testing editor-checker', () => {
-    it('should find VSC', async () => {
-        const res = await checker.findEditor("code");
+    it('should find VSC', () => {
+        const res = checker.findEditor("code");
         assert.equal(res, true);
     })
 
     it('should not find Atom', async () => {
-       const res = await checker.findEditor("atom");
+       const res = checker.findEditor("atom");
        assert.notEqual(res, true);
     })
 })
